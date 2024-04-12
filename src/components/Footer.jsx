c928b985b8ea60f92b31bacda371020fff4b794e
Muhammad Shahzaib <74776486+MShahzaib3242@users.noreply.github.com>
2024-04-12 17:53:46 +0400

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='w-full text-center text-sm tracking-wider text-gray-200 mb-10'>
-        Muhammad Shahzaib | www.mshahzaib.me
+        Muhammad Shahzaib | <a href="https://mshahzaib.me" target='_blank'>www.mshahzaib.me</a>
     </div>
   )
 }
